bc401c12ede58d27f5e5348cde522f83aad63c4a
Jack <Jack>
2020-01-20 14:31:40 +0800

    tomcat

--- src/main/java/com/niepengfei/springboot/MyWebApplicationInitializer.java
+++ src/main/java/com/niepengfei/springboot/MyWebApplicationInitializer.java
@@ -19,14 +19,14 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletCxt) {
-        // Load Spring web application configuration
-        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
-        ac.register(AppConfig.class);
-        //ac.refresh();
-        // Create and register the DispatcherServlet
-        DispatcherServlet servlet = new DispatcherServlet(ac);
-        ServletRegistration.Dynamic registration = servletCxt.addServlet("app", servlet);
-        registration.setLoadOnStartup(1);
-        registration.addMapping("/");
+//        // Load Spring web application configuration
+//        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+//        ac.register(AppConfig.class);
+//        //ac.refresh();
+//        // Create and register the DispatcherServlet
+//        DispatcherServlet servlet = new DispatcherServlet(ac);
+//        ServletRegistration.Dynamic registration = servletCxt.addServlet("app", servlet);
+//        registration.setLoadOnStartup(1);
+//        registration.addMapping("/");
     }
 }
--- src/main/java/com/niepengfei/springboot/SpringApplication.java
+++ src/main/java/com/niepengfei/springboot/SpringApplication.java
@@ -6,7 +6,10 @@ import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.core.StandardEngine;
 import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletRegistration;
 import java.io.File;
 
 /**
@@ -21,10 +24,44 @@ public class SpringApplication {
     public static void run(){
         try{
             Tomcat tomcat = new Tomcat();
-            tomcat.setPort(9090);
-            File file = new File(System.getProperty("java.io.tmpdir"));
-            tomcat.addWebapp("/",file.getAbsolutePath());
+            //File file = new File(System.getProperty("java.io.tmpdir"));
+            //tomcat.addWebapp("/",file.getAbsolutePath());
+
+            Server server = tomcat.getServer();
+            Service service = server.findService("Tomcat");
+
+            Connector connector = new Connector();
+            connector.setPort(9090);
+
+            Engine engine = new StandardEngine();
+            engine.setDefaultHost("localhost");
+
+            Host host = new StandardHost();
+            host.setName("localhost");
+
+            String contextPath = "";
+            Context context = new StandardContext();
+            context.setPath(contextPath);
+            context.addLifecycleListener(new Tomcat.FixContextListener());
+
+            host.addChild(context);
+            engine.addChild(host);
+
+            service.setContainer(engine);
+            service.addConnector(connector);
+
+            AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+            ac.register(AppConfig.class);
+            DispatcherServlet servlet = new DispatcherServlet(ac);
+            Wrapper registration = tomcat.addServlet(contextPath,"app", servlet);
+            //ServletRegistration.Dynamic registration = servletCxt.addServlet("app", servlet);
+            registration.setLoadOnStartup(1);
+            registration.addMapping("/");
+
             tomcat.start();
+
+
+
             tomcat.getServer().await();
         } catch (Exception e){
             System.out.println(e);
